06324d1c3bb7859776f543eef58e92112c5b88af
Alejandro Sanchez <aalejo@gmail.com>
2020-02-26 01:56:57 +0000

    todo list cli

--- src/app.py
+++ src/app.py
@@ -6,27 +6,16 @@ def get_todos():
     return todos
 
 def add_one_task(title):
-    global todos
-    todos.append({ "label": title, "done": False })
+    # your code here
+    pass
 
 def print_list():
-    global todos
-    print("Your current list of tasks ("+str(len(todos))+"):")
-    count = 1
-    for t in todos:
-        status = "✓ done" if t["done"] else "𝙭 pending"
-        print(" "+str(count)+". "+t["label"]+" - "+status)
-        count = count + 1
+    # your code here
+    pass
 
 def delete_task(number_to_delete):
-    global todos
-    new_todos = []
-    number_to_delete = int(number_to_delete) - 1
-    for i in range(0,len(todos)):
-        if str(i) != str(number_to_delete):
-            new_todos.append(todos[i])
-
-    todos = new_todos
+    # your code here
+    pass
 
 def initialize_todos():
     global todos
@@ -42,22 +31,13 @@ def initialize_todos():
 
     
 def save_todos():
-    global todos
-    headers = {'Content-type': 'application/json'}
-    payload = json.dumps(todos)
-    r = requests.put(url = 'https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', data = payload, headers=headers) 
-    response_data = r.json()
-    print(response_data['result'])
-
+    # your code here
+    pass
 
 def load_todos():
-    global todos
-    r = requests.get(url = 'https://assets.breatheco.de/apis/fake/todos/user/alesanchezr') 
-    if(r.status_code == 200):
-        todos = r.json()
-    else:
-        print(r.json())
-
+    # your code here
+    pass
+    
 # Below this code will only run if the entry file running was app.py
 if __name__ == '__main__':
     stop = False
